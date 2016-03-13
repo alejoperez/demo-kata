60d45c1da3b093d679b78d7cea302cca7d65774b
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-13 14:32:37 -0500

    Step2: Add test with different number (Red:Fail)

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -9,3 +9,7 @@ class CalculatorTest(TestCase):
 
     def test_sum_one(self):
         self.assertEqual(Calculator().sum('1'),1,'One number')
+
+    def test_sum_any_number(self):
+        self.assertEqual(Calculator().sum('1'),1,'One string number')
+        self.assertEqual(Calculator().sum('2'),2,'One string number')
