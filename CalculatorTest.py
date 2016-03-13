9454247ff1c9baeb581e5a0311ca2ecd34e5fd5b
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-13 14:49:36 -0500

    Step 4: Add test for any numbers (Red : Fail)

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -16,3 +16,6 @@ class CalculatorTest(TestCase):
 
     def test_sum_two_numbers(self):
         self.assertEqual(Calculator().sum('1,3'),4,'Two string numbers')
+
+    def test_sum_any_numbers(self):
+        self.assertEqual(Calculator().sum('5,2,4,1'),12,'Two string numbers')
