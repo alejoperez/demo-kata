59b90085347b5c96d29057dc475f3ee3ba9520e2
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-13 15:08:25 -0500

    Step 5: Modify sum method to allow different characters

--- Calculator.py
+++ Calculator.py
@@ -4,7 +4,9 @@ class Calculator:
     def sum(self,string):
         if string == '':
             return 0
-        elif ',' in string:
+        elif ',' in string or ':' in string or '&' in string:
+            string = string.replace(':',',')
+            string = string.replace('&',',')
             numbers = string.split(',')
             total = 0
             for num in numbers:
