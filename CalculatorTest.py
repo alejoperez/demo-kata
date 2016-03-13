9aa9a42bf8583fe427b41788352fb3c4339ce696
Alejandro Perez <a.perez15@uniandes.edu.co>
2016-03-13 14:25:45 -0500

    Step2: Create test for one number string (Red: Fail)

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -3,5 +3,9 @@ from unittest import TestCase
 from Calculator import Calculator
 
 class CalculatorTest(TestCase):
+
     def test_sum(self):
-        self.assertEqual(Calculator().sum(''),0,'Cadena Vacia')
+        self.assertEqual(Calculator().sum(''),0,'Empty string')
+
+    def test_sum_one(self):
+        self.assertEqual(Calculator().sum('1'),1,'One number')
